851840499e3a974389da1c4e959c2c441bcbefdc
Kichkun <kichkun@yandex.ru>
2020-06-15 22:16:07 +0300

    split order

--- image_classifier/split_folder.py
+++ image_classifier/split_folder.py
@@ -5,11 +5,11 @@ import shutil
 from tqdm import tqdm_notebook as tqdm
 
 class SplitFolder():
-    def __init__(self, folder, dataset_folder, _split=[0.7,0.15,0.15], class_names={'0':'bad', '1':'bad', '2':'good'}):
+    def __init__(self, folder, dataset_folder, _split=[0.7,0.15,0.15], class_names={'0':'bad', '1':'bad', '2':'good'}, order = {'id':0, 'class':1, 'frame':2, 'else':None}):
         self.folder = folder
         self.dataset_folder = dataset_folder
 
-        _ids = list(set([_file.split('_')[0] for _file in os.listdir(folder)]))
+        _ids = list(set([_file.split('_')[order['id']] for _file in os.listdir(folder)]))
         self.dataset_length = len(_ids)
         random.shuffle(_ids)
         train_len = int(self.dataset_length*_split[0])
@@ -25,7 +25,7 @@ class SplitFolder():
         for key,val in self._ids.items():
            for _id in tqdm(val):
                images = glob.glob(os.path.join(folder, f'{_id}*.jpg'))
-               class_name = images[0].split('_')[-1].split('.')[0]
+               class_name = images[0].split('_')[order['class']]
                if class_names is not None:
                    class_name = class_names[class_name]
                for i, _image in enumerate(images):
